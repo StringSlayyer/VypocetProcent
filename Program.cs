d3ee6fd95183115d26edc92614e54e8f7ac11cdf
StringSlayyer <samuel.zarsky@gmail.com>
2024-05-16 00:28:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Vypocet vypocet = new Vypocet();
+            bool isCounting = true;
+            while (isCounting)
+            {
+                Console.WriteLine("Zadej celkovy pocet hodin");
+                long celkemHodin = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Zadej pocet hodin, ktere jsi chybel");
+                long chybelHodin = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Kolik hodin zbyva do uzavreni znamek?");
+                long zbyvaHodin = Convert.ToInt64(Console.ReadLine());
+                
+                vypocet.VypocetHodin(celkemHodin, chybelHodin, zbyvaHodin);
+                Console.WriteLine("Chceete vypocitat dalsi? 1 - Ano, 2 - Ne");
+                int pokracuj = Convert.ToInt32(Console.ReadLine());
+                switch (pokracuj)
+                {
+                    case 1: isCounting = true; break;
+                    case 2: isCounting = false; break;
+                    default:
+                        Console.WriteLine("spatna volba, pokracujes xd");
+                        isCounting = true; break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Vypocet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Vypocet
+    {
+        
+
+        double Procento(double chybelHodin, long celkemHodin)
+        {
+            return chybelHodin / celkemHodin * 100;
+        }
+
+        public void VypocetHodin(long celkemHodin, long chybelHodin, long zbyvaHodin)
+        {
+            Console.WriteLine("Procento chybejicich hodin ted je: " + Procento(chybelHodin, celkemHodin) + "%.");
+            chybelHodin++;
+            celkemHodin++;
+            Console.WriteLine("Procento zitra: " + Procento(chybelHodin, celkemHodin));
+            Console.WriteLine("zbyva " + zbyvaHodin);
+            if(zbyvaHodin> 1) { 
+            celkemHodin += zbyvaHodin - 1;
+            }
+            Console.WriteLine("promenne po pricteni nepricetni" + chybelHodin + " " + celkemHodin);
+            Console.WriteLine("Procento chybejicich hodin ted bude: " + Procento(chybelHodin, celkemHodin) + "%.");
+
+        }
+    }
+}
